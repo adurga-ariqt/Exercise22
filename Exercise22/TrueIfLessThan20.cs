37883a0ef4fd5f01676949290c6397e58d8f91f1
adurga-ariqt <adurga@ariqt.com>
2022-10-31 12:55:00 +0530

    Add project files.

---
+++ Exercise22/TrueIfLessThan20.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise22
+{
+    internal class TrueIfLessThan20
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInput an integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(result(x));
+        }
+
+        public static bool result(int n)
+        {
+            if (Math.Abs(n - 100) <= 20 || Math.Abs(n - 200) <= 20)
+                return true;
+            return false;
+        }
+    }
+}
